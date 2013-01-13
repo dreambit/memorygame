84ee254e5e7b29166480163079c3afe0b38a2dae
Rezvan Orlechuk <rorlechuk@mirantis.com>
2013-01-13 18:50:00 +0400

    Some classes was added.

--- h/game.h
+++ h/game.h
@@ -1,3 +1,75 @@
+class IGameElement
+{
+public:
+	virtual void Draw() = 0;
+	virtual void Update() = 0;
+	virtual void Restart(){};
+};
+
+
+class GameCondition
+{
+public:
+	GameCondition();
+	GameCondition(char complexity);
+	~GameCondition();
+private:
+	char complexity;
+	uint32 timeleft;
+	uint32 points;
+};
+
+
+class Sprite : public IGameElement
+{
+public:
+	Sprite(char *src);
+	~Sprite();
+protected:
+	void Draw();
+	void Update();
+private:
+	CIw2DImage *image;
+	CIwSVec2 *position;
+	CIwSVec2 *size;
+};
+
+
+class AnimatedSprite : IGameElement
+{
+public:
+	AnimatedSprite();
+	~AnimatedSprite();
+protected:
+	CIwArray<Sprite> *sprites;
+};
+
+
+class SecretBox : public AnimatedSprite
+{
+public:
+	SecretBox();
+	~SecretBox();
+};
+
+
+class SecretBoxArea : public IGameElement
+{
+public:
+	SecretBoxArea();
+	~SecretBoxArea();
+	CIwSVec2 getBoxSize();
+	void Clear();
+	void GenerateBoxes();
+	void Draw();
+	void Update();
+	void Restart();
+private:
+	CIwArray<SecretBox> *secretboxes;
+	uint32 boxescount;
+};
+
+
 class Game
 {
 public:
@@ -5,44 +77,10 @@ public:
     ~Game();
     void Update();
     void Render();
-
+	void NewGame();
+	void AddGameElement(IGameElement *element);
 private:
     CIwSVec2 *size;
+	GameCondition *gameCondition;
+	CIwArray<IGameElement> *gameElements;
 };
-
-
-class DrawableGameElement
-{
-protected:
-	virtual void onDraw() = 0;
-}
-
-
-class UpdatableGameElement
-{
-protected:
-	virtual void onUpdate() = 0;
-}
-
-
-class Sprite : public DrawableGameElement, UpdatableGameElement
-{
-public:
-	Sprite(char *src);
-protected:
-	void onDraw();
-	void onUpdate();
-private:
-	CIw2DImage *image;
-	CIwSVec2 *position;
-	CIwSVec2 *size;
-}
-
-
-class AnimatedSprite : DrawableGameElement, UpdatableGameElement
-{
-public:
-	AnimatedSprite();
-protected:
-	CIwArray<Sprite> *sprites;
-}
--- src/game.cpp
+++ src/game.cpp
@@ -2,24 +2,29 @@
 #include "Iw2D.h"
 #include "game.h"
 
-
-Game::Game()
+GameCondition::GameCondition()
 {
-}
-
-
-Game::~Game()
-{
+	complexity = 1;
+	timeleft = 50000;
+	points = 0;
 }
 
 
 Sprite::Sprite(char *src)
 {
 	image = Iw2DCreateImage(src);
+	position = new CIwSVec2(0, 0);
+	size = new CIwSVec2(128, 128);
 }
 
 
-void Sprite::onDraw()
+Sprite::~Sprite()
+{
+	delete image;
+}
+
+
+void Sprite::Draw()
 {
 	Iw2DDrawImage(image, *position, *size);
 }
@@ -28,4 +33,61 @@ void Sprite::onDraw()
 AnimatedSprite::AnimatedSprite()
 {
 	sprites = new CIwArray<Sprite>();
+}
+
+
+AnimatedSprite::~AnimatedSprite()
+{
+	sprites->clear();
+	delete sprites;
+}
+
+
+Game::Game()
+{
+	size = new CIwSVec2();
+	gameCondition = new GameCondition();
+	gameElements = new CIwArray<IGameElement>();
+}
+
+
+Game::~Game()
+{
+	delete size;
+	delete gameCondition;
+	delete gameElements;
+}
+
+
+void Game::Render()
+{
+	for (CIwArray<IGameElement>::iterator it = gameElements->begin(); it != gameElements->end(); it++)
+	{
+		it->Draw();
+	}
+}
+
+
+void Game::Update()
+{
+	for (CIwArray<IGameElement>::iterator it = gameElements->begin(); it != gameElements->end(); it++)
+	{
+		it->Update();
+	}
+}
+
+
+void Game::NewGame()
+{
+	for (CIwArray<IGameElement>::iterator it = gameElements->begin(); it != gameElements->end(); it++)
+	{
+		it->Restart();
+	}
+
+}
+
+
+void Game::AddGameElement(IGameElement *element)
+{
+	gameElements->push_back(*element);
 }
